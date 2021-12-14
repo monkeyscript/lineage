e29bdbc3f470c2ecf9f545121f17f938005a448a
MonkeyScript <monkeyscript.dev@gmail.com>
2021-12-14 18:26:28 +0530

    Data moved to firebase

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { List } from '../../data/list';
 import { Node, Edge } from '@swimlane/ngx-graph';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +11,13 @@ import { Observable } from 'rxjs';
 export class HomeComponent implements OnInit {
 
   // List of all individuals
-  data = List;
+  people : {
+    id        :   string,
+    name      :   string,
+    parent    :   string[],
+    birthday  :   string,
+    image     :   string,
+  }[] = [];
 
   // Nodes for the graph
   nodes : Node[] = [];
@@ -29,53 +34,67 @@ export class HomeComponent implements OnInit {
     date: string
   }[] = [];
 
+  // For triggering graph update 
+  update$: Subject<boolean> = new Subject();
+
   constructor(
     private firestore: AngularFirestore
   ) { }
 
   ngOnInit(): void {
 
-    let items = this.firestore.collection('people').valueChanges();
-    items.subscribe(result=>console.log(result))
-
-    // Iterate over data 
-    this.data.forEach(
-      person => {
-        this.nodes.push({
-          id    : person.id,
-          label : person.name,
-        })
-        person.parent.forEach(
-          parent => {
-            this.edges.push({
-              id      : parent + '_' + person.id,
-              source  : parent,
-              target  : person.id
-            })
+    // Load data from firestore
+    let people = this.firestore.collection('people').valueChanges();
+    people.subscribe((result:any[])=> {
+      this.people = result;
+      
+      // Iterate over people 
+      this.people.forEach(
+        person => {
+          let nodeObject :any = {
+            id    : person.id,
+            label : person.name,
+            image : person.image
           }
-        )
-      }
-    )
-    
-    // Find upcoming birthdays
-    let currentDate = new Date(); 
-    // Iterate over people and check date 
-    this.data.forEach(person => {
-      let bday = new Date(person.bday);
-      bday.setFullYear(currentDate.getFullYear());
-      if(person.bday!=='' && bday > currentDate) {
-        this.upcomingBirthdays.push({
-          name : person.name,
-          date : new Date(bday).toLocaleDateString('en-us', {weekday:"long", month:"short", day:"numeric"}) 
-        })
-      }
+          this.nodes.push(nodeObject);
+          if(person.parent) {
+            person.parent.forEach(
+              parent => {
+                this.edges.push({
+                  id      : parent + '_' + person.id,
+                  source  : parent,
+                  target  : person.id
+                })
+              }
+            )
+          }
+        }
+      )
+
+      // Find upcoming birthdays
+      let currentDate = new Date(); 
+      // Iterate over people and check date 
+      this.people.forEach(person => {
+        let bday = new Date(person.birthday);
+        bday.setFullYear(currentDate.getFullYear());
+        if(person.birthday!=='' && bday > currentDate) {
+          this.upcomingBirthdays.push({
+            name : person.name,
+            date : new Date(bday).toLocaleDateString('en-us', {weekday:"long", month:"short", day:"numeric"}) 
+          })
+        }
+      });
+
+      // Update graph 
+      this.update$.next(true);
+      
     });
 
   }
 
   // On node click
   onNodeSelect(node:Node) {
-    let selectedNode = this.data.find(person=>person.id===node.id);
+    let selectedNode = this.people.find(person=>person.id===node.id);
     if(selectedNode) {
       this.selectedNode = selectedNode;
     }
